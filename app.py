5825d94987c5e1a312a8cf0a4b99255ea713512f
Debadritnag04 <22642723068.bca@ticollege.org>
2026-03-29 18:03:57 +0530

    GPAC

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 # Load the Gemini model
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 @app.route("/", methods=["GET"])
